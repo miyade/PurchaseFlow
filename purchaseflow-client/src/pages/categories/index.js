90c021769ea304aa8c71786c0eb5e6e68355c151
miyade <mariem.ayedi@esprit.tn>
2020-12-02 12:43:42 +0100

    selected category/ modal

--- purchaseflow-client/src/pages/categories/index.js
+++ purchaseflow-client/src/pages/categories/index.js
@@ -1,33 +1,66 @@
-import React, { useEffect, useState} from 'react';import { Container, Button, Form, FormGroup, Input } from 'reactstrap';
+import React, { useEffect, useState} from 'react';
+import { Modal, ModalHeader, ModalBody, ModalFooter, Container, Button, Form, FormGroup, Input } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import api from '../../services/api';
 
 export default function Welcome({history}){
+
     const [categories, setCategories] = useState([]);
+    const [selected, setSelected] = useState();
+    const [modal, setModal] = useState(false);
+    const [selectedCategory, setSelectedCategory] = useState("");
+
     const apiURL= '/v1/product_categories';
     useEffect(() => {
         getEvents()
     },[])
 
+    const toggle = () => setModal(!modal);
 
+    
+    const selectHandler = async (selectedItem) => {
+      // console.log('this works');
+       setSelected(true);
+       setSelectedCategory(selectedItem);
+    }
 
    const getEvents = async () => {
       
         const response= await api.get(apiURL)
         setCategories(response.data.data)
    };
-   console.log(categories)
+   //console.log(categories)
 
     return (
         <div>
             <ul className="categories-list">
                 {categories.map(category => (
                     <li key={category.id}>
-                    <strong>{category.attributes.name}</strong>
+                    <strong onClick={() => selectHandler(category.attributes.name)} >{category.attributes.name}</strong>
                     </li>
                      ))}
             </ul>
-            <Button>Next</Button> 
+            {
+                selected ? (
+                    <Button onClick={toggle}> Next </Button>
+                ) : ""
+            }
+           
+            <Modal isOpen={modal} toggle={toggle}>
+            <ModalHeader toggle={toggle}>Contact Information</ModalHeader>
+            <ModalBody>
+            
+            <Input type="checkbox" />{' '}
+              
+                Accept <a href="">Terms & Conditions</a>
+
+            </ModalBody>
+            <ModalFooter>
+            <Button color="primary" onClick={toggle}>Next</Button>{' '}
+            <Button color="secondary" onClick={toggle}>Cancel</Button>
+            </ModalFooter>
+            </Modal>
+
         </div>
        
     )
