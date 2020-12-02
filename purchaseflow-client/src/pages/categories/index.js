2d0e7a4a88e06c6d2ffc4a933f906b1651b985f8
miyade <mariem.ayedi@esprit.tn>
2020-12-02 13:25:31 +0100

    Modal contact info

--- purchaseflow-client/src/pages/categories/index.js
+++ purchaseflow-client/src/pages/categories/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState} from 'react';
-import { Modal, ModalHeader, ModalBody, ModalFooter, Container, Button, Form, FormGroup, Input } from 'reactstrap';
+import {Label, Modal, ModalHeader, ModalBody, ModalFooter, Container, Button, Form, FormGroup, Input } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import api from '../../services/api';
 
@@ -9,6 +9,17 @@ export default function Welcome({history}){
     const [selected, setSelected] = useState();
     const [modal, setModal] = useState(false);
     const [selectedCategory, setSelectedCategory] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState();
+    const [emailAddress, setEmailAddress] = useState("");
+    const [success, setSuccess] = useState(false)
+    const [error, setError] = useState(false)
+
+
+
+
+
 
     const apiURL= '/v1/product_categories';
     useEffect(() => {
@@ -22,6 +33,42 @@ export default function Welcome({history}){
       // console.log('this works');
        setSelected(true);
        setSelectedCategory(selectedItem);
+    }
+
+
+    const submitHandler = async (evt) => {
+        evt.preventDefault()
+
+        const contactData = new FormData();
+
+        contactData.append("firstName", firstName)
+        contactData.append("lastName", lastName)
+        contactData.append("emailAddress", emailAddress)
+        contactData.append("phoneNumber", phoneNumber)
+        contactData.append("selectedCategory", selectedCategory)
+        try {
+            if (firstName !== "" &&
+                lastName !== "" &&
+                emailAddress !== "" &&
+                phoneNumber !== null &&
+                selectedCategory !== "" 
+              
+            ) {
+                setSuccess(true)
+                setTimeout(() => {
+                    setSuccess(false)
+                    history.push("/")
+                }, 2000)
+            } else {
+                setError(true)
+                setTimeout(() => {
+                    setError(false)
+                }, 2000)
+            }
+        } catch (error) {
+            Promise.reject(error);
+            console.log(error);
+        }
     }
 
    const getEvents = async () => {
@@ -50,14 +97,37 @@ export default function Welcome({history}){
             <ModalHeader toggle={toggle}>Contact Information</ModalHeader>
             <ModalBody>
             
-            <Input type="checkbox" />{' '}
-              
-                Accept <a href="">Terms & Conditions</a>
+            <Form onSubmit={submitHandler}>
+                    <FormGroup>
+                        <Label>First Name: </Label>
+                        <Input id="title" type="text" value={firstName} placeholder={'First Name ..'} onChange={(evt) => setFirstName(evt.target.value)} />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>Last Name: </Label>
+                        <Input id="description" type="text" value={lastName} placeholder={'Last Name ..'} onChange={(evt) => setLastName(evt.target.value)} />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>Email Address: </Label>
+                        <Input id="price" type="text" value={emailAddress} placeholder={'Email Address .. xyz@azer.xy'} onChange={(evt) => setEmailAddress(evt.target.value)} />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label>Phone Number: </Label>
+                        <Input id="date" type="text" value={phoneNumber} placeholder={'Phone Number.. '} onChange={(evt) => setPhoneNumber(evt.target.value)} />
+                    </FormGroup>
+            </Form>
 
+                <Input type="checkbox" />{' '}
+                Accept <a href="">Terms & Conditions</a>
             </ModalBody>
             <ModalFooter>
-            <Button color="primary" onClick={toggle}>Next</Button>{' '}
-            <Button color="secondary" onClick={toggle}>Cancel</Button>
+            <FormGroup>
+                    <Button className="submit-btn" >Submit</Button>
+                </FormGroup>
+                <FormGroup>
+                    <Button className="secondary-btn" onClick={() => toggle()}>
+                        Cancel
+                    </Button>
+                </FormGroup>
             </ModalFooter>
             </Modal>
 
